90a3c933de64f64ae2d8cd4f8c35334d3120d8bd
shakil <hasan1707058@stud.kuet.ac.bd>
2021-11-17 09:03:28 +0600

    JavaClass, javaClass1 is added

---
+++ app/javaClass1.java
@@ -0,0 +1,6 @@
+public class javaClass1 {
+    private String number;
+    public javaClass1(String var) {
+        this.number = var;
+    }
+}
---
+++ app/src/main/java/com/example/loginpage/ui/login/JavaClass.java
@@ -0,0 +1,5 @@
+package com.example.loginpage.ui.login;
+
+public class JavaClass {
+
+}
